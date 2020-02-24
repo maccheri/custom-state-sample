51df160bccdae326d1f3070df12081c14b62e4dd
Felipe Maccheri <felipemaccheri@gmail.com>
2020-02-24 18:18:26 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useMemo } from 'react';
 import './App.css';
 
-function App() {
+const useDocumentTitle = (title) => {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+}
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const title = useMemo(() => {
+    console.log(data);
+    const pendingTodos = data.filter((todo) => !todo.completed).length;
+    return `You have ${pendingTodos} pending todos!`;
+  }, [data]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  useDocumentTitle(title);
+
+  const onClick = (todo) => {
+    const completed = !todo.completed;
+    const newTodo = { ...todo, completed };
+    const newData = data.map(t => {
+      if (t.id === todo.id && t.userId === todo.userId) {
+        return newTodo;
+      }
+      return t;
+    });
+
+    setData(newData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos - {title}</h1>
+      {data && data.map((todo) => {
+        const classes = `todo ${todo.completed ? 'done' : 'pending'}`;
+        return (
+          <div className={classes} key={todo.id} onClick={() => onClick(todo)}>
+            {todo.title}
+          </div>
+        )
+      })}
     </div>
   );
 }
